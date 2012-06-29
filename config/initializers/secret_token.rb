f1048abba0809396bbe74b85232bb4329d902a45
segfault <s9gf4ult@gmail.com>
2012-06-29 11:38:54 +0600

    add everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Judgement::Application.config.secret_token = 'd10c3e6ac61a174a429c5941b64016eee69569a5a5e111970361f289b85b5b4689507e308811c4348343f07c7777726b18c40503ac3b439110b52e40941da24a'
